e1a4c7c5a7a6cff746cc58c2f898e257d307461d
Mersikka <mio@argillander.me>
2025-11-16 17:32:08 +0200

    Add info element

--- src/util.py
+++ src/util.py
@@ -7,3 +7,9 @@ def validate_todo(content):
 
     if len(content) > 100:
           raise UserInputError("Todo content length must be smaller than 100")
+
+def split_names(content):
+    if content["author"].find(" and ") == -1:
+        return
+    name_list = content["author"].split(" and ")
+    content["author"] = name_list
