204284758aae920d8438de3516ce3fe5003e5315
JoonaPietarinen <joonapietarinen3@gmail.com>
2025-11-16 15:23:29 +0200

    Added author and citations_authors tables

--- src/schema.sql
+++ src/schema.sql
@@ -7,11 +7,21 @@ CREATE TABLE citations (
   id SERIAL PRIMARY KEY,
   type TEXT NOT NULL,            -- esim. 'book', 'article', 'webpage'
   title TEXT NOT NULL,
-  author TEXT,
   year INTEGER,
   publisher TEXT,
   isbn TEXT,
   doi TEXT,
   url TEXT,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE authors (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE citations_authors (
+  citation_id INTEGER REFERENCES citations(id) ON DELETE CASCADE,
+  author_id INTEGER REFERENCES authors(id) ON DELETE CASCADE,
+  author_order INTEGER, 
+  PRIMARY KEY (citation_id, author_id)
 );
