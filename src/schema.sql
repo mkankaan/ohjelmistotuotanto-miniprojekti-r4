aba95c085703bee0693907d5be953bbed33f92a8
JoonaPietarinen <joonapietarinen3@gmail.com>
2025-11-16 14:27:23 +0200

    Changed books to citations

--- src/schema.sql
+++ src/schema.sql
@@ -3,12 +3,15 @@ CREATE TABLE todos (
   content TEXT NOT NULL,
   done BOOLEAN DEFAULT FALSE
 );
-CREATE TABLE books (
+CREATE TABLE citations (
   id SERIAL PRIMARY KEY,
+  type TEXT NOT NULL,            -- esim. 'book', 'article', 'webpage'
   title TEXT NOT NULL,
-  author TEXT NOT NULL,
+  author TEXT,
   year INTEGER,
   publisher TEXT,
   isbn TEXT,
+  doi TEXT,
+  url TEXT,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
