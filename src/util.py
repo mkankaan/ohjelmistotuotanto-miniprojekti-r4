136c8ae6d1398ae9b6aa8568a8205284a88436f4
mkankaan <104016919+mkankaan@users.noreply.github.com>
2025-11-18 18:08:30 +0200

    Add unit tests for util/format_authors

--- src/util.py
+++ src/util.py
@@ -6,7 +6,7 @@ def validate_todo(content):
         raise UserInputError("Todo content length must be greater than 4")
 
     if len(content) > 100:
-          raise UserInputError("Todo content length must be smaller than 100")
+        raise UserInputError("Todo content length must be smaller than 100")
 
 def split_names(content):
     if content["author"].find(" and ") == -1:
