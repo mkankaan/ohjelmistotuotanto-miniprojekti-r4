f903b763997802b966ec78d33136c28e4c5a5ba6
JoonaPietarinen <joonapietarinen3@gmail.com>
2025-11-19 14:33:19 +0200

    Added test for split_name

---
+++ src/tests/split_name_test.py
@@ -0,0 +1,16 @@
+import unittest
+from util import split_names
+
+class TestSplitNames(unittest.TestCase):
+    def test_single_author(self):
+        content = {"author": "Example 1"}
+        split_names(content)
+        self.assertEqual(content["author"], ["Example 1"])
+
+    def test_multiple_authors(self):
+        content = {"author": "Example 1 and Example 2"}
+        split_names(content)
+        self.assertEqual(content["author"], ["Example 1", "Example 2"])
+
+if __name__ == "__main__":
+    unittest.main()
