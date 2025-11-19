8c9c83d1099690fdc6f8dbc373dcd1d2bef0e195
mersikka <mio@argillander.me>
2025-11-19 18:44:02 +0200

    Refactor format_authors() Also removed an unneeded file

--- src/util.py
+++ src/util.py
@@ -1,13 +1,6 @@
 class UserInputError(Exception):
     pass
 
-def validate_todo(content):
-    if len(content) < 5:
-        raise UserInputError("Todo content length must be greater than 4")
-
-    if len(content) > 100:
-        raise UserInputError("Todo content length must be smaller than 100")
-
 def split_names(content):
     if content["author"].find(" and ") == -1:
         content["author"] = [content["author"]]
@@ -16,13 +9,4 @@ def split_names(content):
     content["author"] = name_list
 
 def format_authors(author_list):
-    formatted_string = ""
-
-    for i in range(len(author_list)):
-        formatted_string += author_list[i]
-        print(author_list[i])
-
-        if i < len(author_list)-1:
-            formatted_string += " and "
-
-    return formatted_string
+    return " and ".join(author_list)
