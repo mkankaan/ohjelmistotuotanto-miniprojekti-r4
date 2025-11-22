4f59e5314486e3325f46f994c7e13a0c3d8357ee
Sofia Kettunen <sofia.kettunen@helsinki.fi>
2025-11-22 20:17:05 +0200

    Check form validity on any input
    Removed id attributes from input fields since they're now unnecessary

--- src/static/new_citation.js
+++ src/static/new_citation.js
@@ -2,10 +2,8 @@ const form = document.forms[0];
 const create_button = document.getElementById("create");
 create_button.disabled = true;
 
-const possibly_invalid_input_ids = ["title", "year"];
-
-for (const id of possibly_invalid_input_ids) {
-    document.getElementById(id).addEventListener("input", () => {
+for (const input of form.elements) {
+    input.addEventListener("input", () => {
         create_button.disabled = !form.checkValidity();
     });
 }
