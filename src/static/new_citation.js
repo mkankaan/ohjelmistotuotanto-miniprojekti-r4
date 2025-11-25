defa281d962495479082ddcb4f4f6dc6c3cfd12b
Mersikka <mio@argillander.me>
2025-11-26 01:10:20 +0200

    Improve event checking

--- src/static/new_citation.js
+++ src/static/new_citation.js
@@ -2,10 +2,44 @@ const form = document.forms[0];
 const create_button = document.getElementById("create");
 const citationKeyInput = form.elements["citation_key"];
 const errorSpan = document.getElementById("ck-error");
-
+const doi_form = document.forms["doi-populate-form"]
+const populate_button = document.getElementById("submit-doi")
+populate_button.disabled = true;
 create_button.disabled = true;
 
 let lastCheckedKey = "";
+
+
+const typeOptions = [
+    { value: 'article', label: 'Article' },
+    { value: 'book', label: 'Book' },
+    { value: 'inproceedings', label: 'Conference' },
+    { value: 'book-chapter', label: 'Book Chapter' },
+    { value: 'misc', label: 'Other' }
+];
+
+
+const updateButtonState = () => {
+    create_button.disabled = !form.checkValidity();
+    populate_button.disabled = !doi_form.checkValidity();
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const selectElement = document.getElementById('type');
+    typeOptions.forEach(option => {
+        const opt = document.createElement('option');
+        opt.value = option.value;
+        opt.textContent = option.label;
+        selectElement.appendChild(opt);
+
+    updateButtonState()
+    });
+});
+
+
+document.addEventListener("change", updateButtonState)
+
 
 citationKeyInput.addEventListener("input", async function() {
     const key = this.value;
@@ -36,36 +70,18 @@ citationKeyInput.addEventListener("input", async function() {
 });
 
 
-
 for (const input of form.elements) {
     if (input !== citationKeyInput) {
         input.addEventListener("input", updateButtonState);
     }
 }
 
-function updateButtonState() {
-    create_button.disabled = !form.checkValidity();
-}
-
-
-const doi_form = document.forms["doi-populate-form"]
-const populate_button = document.getElementById("submit-doi")
-populate_button.disabled = true;
 
 for (const doi_input of doi_form.elements) {
     doi_input.addEventListener("input", () => {
-        populate_button.disabled = !doi_form.checkValidity();
+        doi_input.addEventListener("input", updateButtonState);
     });
 }
-
-
-const typeOptions = [
-    { value: 'article', label: 'Article' },
-    { value: 'book', label: 'Book' },
-    { value: 'inproceedings', label: 'Conference' },
-    { value: 'book-chapter', label: 'Book Chapter' },
-    { value: 'misc', label: 'Other' }
-];
 
 
 document.getElementById('doi-populate-form').addEventListener('submit', function(e) {
@@ -91,19 +107,10 @@ document.getElementById('doi-populate-form').addEventListener('submit', function
         document.getElementById('isbn').value = data.isbn;
         document.getElementById('doi').value = data.doi;
         document.getElementById('url').value = data.url;
+    })
+    .then(() => {
+        document.dispatchEvent(new Event('change', { bubbles: true }))
     });
 });
 
 
-document.addEventListener('DOMContentLoaded', () => {
-    populate_button.disabled = !doi_form.checkValidity();
-    create_button.disabled = !form.checkValidity();
-
-    const selectElement = document.getElementById('type');
-    typeOptions.forEach(option => {
-        const opt = document.createElement('option');
-        opt.value = option.value;
-        opt.textContent = option.label;
-        selectElement.appendChild(opt);
-    });
-});
